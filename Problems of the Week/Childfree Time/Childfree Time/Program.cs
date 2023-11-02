6fa19347bc078827f5019f9490544d07a8b72d62
Sandeep Singh <sandeepchahal433@gmail.com>
2023-11-02 21:15:40 +0530

    recursion corrected

--- Problems of the Week/Childfree Time/Childfree Time/Program.cs
+++ Problems of the Week/Childfree Time/Childfree Time/Program.cs
@@ -1,68 +1,77 @@
-﻿string info = Console.ReadLine();
+﻿using System.Diagnostics;
+
+string info = Console.ReadLine();
 int n = Convert.ToInt32(info.Split(" ")[0]);
 int p = Convert.ToInt32(info.Split(" ")[1]);
 
 List<string> list = new List<string>(); 
 Dictionary<string,int> cache = new Dictionary<string,int>();
-for (int i = 0; i < n; i++)
+Stopwatch s = new Stopwatch();
+s.Start();
+for (int i = 0; i < p; i++)
 {
     list.Add(Console.ReadLine());
 }
-
+s.Stop();
+Console.WriteLine($"elapsed {s.Elapsed.TotalSeconds}");
+s.Start();
+var sums = new List<int>();
 foreach (var item in list)
 {
-    string parentKey = item.Split(' ')[0] + item.Split(' ')[1];
-    List<int> sums = new List<int>();
+    var split = item.Split(" ");
+    var parentKey = split[0] +"-"+ split[1];
+    
     if(!cache.ContainsKey(parentKey))
     {
-        var relatedListItems = list.Where(col => col.Split(" ")[0] == parentKey[1].ToString()).ToList();
-
+        var relatedListItems = list.Where(col => col.Split(" ")[0] == parentKey.Split("-")[1]).ToList();
         if (relatedListItems.Count != 0)
         {
-            cache[parentKey]= 2 + Convert.ToInt32(item.Split(' ')[2]);
-           
-            foreach (var relatedItem in relatedListItems)
-            {
-                sums.Add(CalculatePortal(relatedItem, cache, list));
-            }
+            cache[parentKey]= 2 + Convert.ToInt32(split[2]);
+
+            sums.AddRange(relatedListItems.Select(relatedItem => CalculatePortal(relatedItem, cache, list)));
 
             cache[parentKey] += sums.Max();
-
-        }
-    }
-}
-
-Console.WriteLine(cache.OrderByDescending(col => col.Value).First().Value);
-Console.ReadLine();
-
-static int CalculatePortal(string item, Dictionary<string, int> dic, List<string> list)
-{
-    string key = item.Split(' ')[0] + item.Split(' ')[1];
- 
-    if (!dic.ContainsKey(key))
-    {
-        dic[key] = 2 + Convert.ToInt32(item.Split(' ')[2]);
-
-        var relatedListItems = list.Where(col => col.Split(" ")[0] == key[1].ToString()).ToList(); // it will give the items starting from where last ended
-
-        if (relatedListItems.Count != 0)
-        {
-            foreach (var relatedItem in relatedListItems)
-            {
-                int count = CalculatePortal(relatedItem, dic, list);
-
-                dic[key] += count;
-               
-            }
-            return dic[key];
+            sums.Clear();
         }
         else
         {
-            return dic[key];
+            cache[parentKey]= 2 + Convert.ToInt32(split[2]);
         }
     }
-    else
+}
+s.Stop();
+Console.WriteLine($"Found it  {s.Elapsed.TotalSeconds}");
+Console.WriteLine($"max {cache.OrderByDescending(col => col.Value).First().Value}");
+
+
+static int CalculatePortal(string item, Dictionary<string, int> dic, List<string> list)
+{
+    var split = item.Split(" ");
+    string key = split[0] +"-"+ split[1];
+ 
+    switch (dic.ContainsKey(key))
     {
-        return dic[key];
+        case false:
+        {
+            dic[key] = 2 + Convert.ToInt32(split[2]);
+
+            var relatedListItems = list.Where(col => col.Split(" ")[0] == key.Split("-")[1]).ToList(); // it will give the items starting from where last ended
+
+            if (relatedListItems.Count != 0)
+            {
+                foreach (var relatedItem in relatedListItems)
+                {
+                    int count = CalculatePortal(relatedItem, dic, list);
+
+                    dic[key] += count;
+               
+                }
+                return dic[key];
+            }
+
+            return dic[key];
+        }
+        default:
+            return dic[key];
     }
 }
