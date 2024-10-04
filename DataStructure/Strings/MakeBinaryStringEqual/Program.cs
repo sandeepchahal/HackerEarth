71f5b6fc4a56894f4e7c465407b22ee2634e072e
sandeepsingh <sandeepchahal433@gmail.com>
2024-10-04 09:42:39 +0530

    More problems solved

---
+++ DataStructure/Strings/MakeBinaryStringEqual/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+B obj = new B();
+obj.Print("Hello");
+
+public abstract class A
+{
+    public virtual void Print(string msg)
+    {
+        Console.WriteLine(msg);
+    }
+}
+
+public class B : A
+{
+    public override void Print(string msg)
+    {
+        base.Print(msg:msg);
+        Console.WriteLine("This is derived class");
+    }
+}
+
+
